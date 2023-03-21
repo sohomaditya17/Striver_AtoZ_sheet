f09a0ea10f14c6f3762b4f99599dd3f957dc0e98
sohommzxu <sohom1947@gmail.com>
2023-03-22 01:35:41 +0530

    test

--- Pattern2.java
+++ Pattern2.java
@@ -3,7 +3,7 @@ class Pattern2
 {
     public static void main(String args[])
     {
-        for(int i=0;i<5;i++)
+        for(int i=0;i<=4;i++)
         {
             for(int j=0;j<=i;j++)
             {
