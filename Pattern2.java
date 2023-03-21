74b285807e29760b560d860cb5fb61816089098d
sohommzxu <sohom1947@gmail.com>
2023-03-22 00:49:14 +0530

    test

--- Pattern2.java
+++ Pattern2.java
@@ -3,11 +3,11 @@ class Pattern2
 {
     public static void main(String args[])
     {
-        for(int i=1;i<=5;i++)
+        for(int i=0;i<5;i++)
         {
-            for(int j=1;j<=i;j++)
+            for(int j=0;j<=i;j++)
             {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
         }
