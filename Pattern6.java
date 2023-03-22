c3063638ff56bb4e1ec9c8be9edea387a893b4c9
sohommzxu <sohom1947@gmail.com>
2023-03-22 20:24:37 +0530

    test

--- Pattern6.java
+++ Pattern6.java
@@ -1,10 +1,15 @@
-class Pattern6 {
-    for(int i=1;i<=5;i++)
+class Pattern6 
+{
+    public static void main(String args[])
     {
-        for(int j=5;j>=i;j--)
+        for(int i=1;i<=5;i++)
         {
-            System.out.print(i+" ");
+            for(int j=5;j>=i;j--)
+            {
+                System.out.print(5-j+1+" ");
+            }
+        System.out.println();
         }
-    System.out.println();
     }
+
 }
