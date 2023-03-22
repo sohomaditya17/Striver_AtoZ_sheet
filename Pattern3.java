94d3655a01906dcaf0d7b7560d4726bc82e82cf8
sohommzxu <sohom1947@gmail.com>
2023-03-22 18:59:10 +0530

    test

--- Pattern3.java
+++ Pattern3.java
@@ -5,11 +5,11 @@ class Pattern3
     {
         for(int i=1;i<=5;i++)
         {
-            for(int j=1;j<=i;i++)
-                System.out.print("1");
-        }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j+" ");
+            }
         System.out.println();
+        }   
     }
-
-    
 }
