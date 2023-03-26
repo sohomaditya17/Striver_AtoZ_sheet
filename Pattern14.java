7cee4768e76deb4bfd7bd69d6c81ccbb7f8d9259
sohommzxu <sohom1947@gmail.com>
2023-03-26 18:35:44 +0530

    test

---
+++ Pattern14.java
@@ -0,0 +1,13 @@
+class Pattern14 {
+    public static void main(String args[])
+    {
+        for(int i=0;i<5;i++)
+        {
+            for(char ch='A';ch<='A'+i;ch++)
+            {
+                System.out.print(ch+" ");
+            }
+        System.out.println();
+        }
+    }
+}
